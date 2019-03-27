56ebed446901caf4e58b25f93f735769561d506d
Fernando Lujan <fernando@Fernandos-MacBook-Pro.local>
2019-03-27 16:06:18 -0700

    Locally builded

---
+++ build/precache-manifest.aca60622ec0d4bd393ccd3e707dd0e9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a7f1f31000fe080e0f0",
+    "url": "/static/css/main.194433fe.chunk.css"
+  },
+  {
+    "revision": "7a7f1f31000fe080e0f0",
+    "url": "/static/js/main.46fcd87f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "83446ba81ca3b735c35d7d7637fecf30",
+    "url": "/index.html"
+  }
+];
