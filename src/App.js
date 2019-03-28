48a750a50e5be96819ac8de7de83bbaa04022b2e
Fernando Lujan <fernando@Fernandos-MacBook-Pro.local>
2019-03-28 12:17:43 -0700

    changed app.js

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ class App extends Component {
     fetch("/test")
     .then(res => {
       if (!res.ok) { throw res }
+      console.log(res);
       res.json()
       })
     .then( (result => {
-      // console.log(result);
+      console.log(result);
       this.setState({ text: result.text})
     }))
     .catch(err => console.log(err))
